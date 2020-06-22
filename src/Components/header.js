b01240b7a15dfd8cd24067c0fea645f48375f54c
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2020-06-22 02:33:09 -0700

    Update Header.js File for Expenso App

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="header">
-            <img src="/images/logo.png" alt="Expenso Logo" className="Logo"/>
+            <img src="../images/logo.png" alt="Expenso Logo" className="Logo"/>
         </div>
     )
 }
