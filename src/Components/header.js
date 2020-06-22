f26b7a1d064138b4fcfd578798e5a91db877cadd
Muhammad Aashir Khan <55949359+AashirKhan21@users.noreply.github.com>
2020-06-22 02:05:03 -0700

    Made Header component and style in css

---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="/images/logo.png" alt="Expenso Logo" className="Logo"/>
+        </div>
+    )
+}
+
+export default Header
